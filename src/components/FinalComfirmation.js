5a8188183b4c956f070ed7e9b067ce18a789a461
Tolisz <anatolii.radionov2002@gmail.com>
2023-01-19 23:26:18 +0100

    Confirmation

--- src/components/FinalComfirmation.js
+++ src/components/FinalComfirmation.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
+import { Circles } from 'react-loader-spinner'
 
 import { protectedResources } from "../authConfig";
 import useFetchWithMsal from '../hooks/useFetchWithMsal';
@@ -16,31 +17,44 @@ const FinalComfirmation = () => {
         scopes: protectedResources.apiLoanComparer.scopes.read,
     });
 
-	useEffect(() => {
+	const [confirmed, setConfirmed] = useState(false);
 
+	useEffect(() => {
 		CheckConfirmation();
 
+		// eslint-disable-next-line
 	}, [])
 
 	const CheckConfirmation = () => 
 	{
-		console.log("Dzień dobry")
+		console.log("Dzień dobry");
 
 		try{
-
-			execute("GET", protectedResources.apiLoanComparer.endpoint `getFinalConfirmation/${rqid}/${guid}`)
+			execute("GET", protectedResources.apiLoanComparer.endpoint + `getFinalConfirmation/${rqid}/${guid}`)
 			.then((response) =>
 			{
+
+				if (response.status === 200 || response.status === 204)
+				{
+					setConfirmed(true);
+				}
+				else
+				{
+					setConfirmed(false);
+				}
+
 				console.log(response);
 			})
 			.catch((e) => {
 				console.log("BŁAD = ", e);
+				setConfirmed(false);
 			}
 			)
 		}
 		catch(e)
 		{
 			console.log("Wyjątek = ", e);
+			setConfirmed(false);
 		}
 	}
 
@@ -49,10 +63,34 @@ const FinalComfirmation = () => {
 		<div className='FinalComf-island'>
 			<div className='FinalComf-whole' >
 				<h2>Akceptacja wniosku!</h2>
-				<p>
-					Gratulujemy! Twój wniosek kredytowy został zaakceptowany poprzez pracownika banku.
-					Serdecznie zapraszamy po odbior pieniędzy do banku.
-				</p>
+
+				{
+					isLoading
+						?
+					<Circles 
+						height="80"
+						width="80"
+						color="#4fa94d"
+						ariaLabel="circles-loading"
+						wrapperStyle={{ margin: 25}}
+						wrapperClass=""
+						visible={true}
+					/>
+						:
+					(
+						confirmed
+							?
+						<p>
+							Gratulujemy! Twój wniosek kredytowy został zaakceptowany poprzez pracownika banku.
+							Serdecznie zapraszamy po odbior pieniędzy do banku.
+						</p>
+							:
+						<p>
+							Twój wnisek nie jest zaakceptowany
+						</p>
+					)
+
+				}
 			</div>
 		</div>
 	)
